b56e5ab0554f1f108697b23a60a0c41191ca2999
github keita7777 <github mine0kei@gmail.com>
2024-11-23 06:34:52 +0900

    不要なdivタグ削除

--- components/Bookmark/CreateButton/index.tsx
+++ components/Bookmark/CreateButton/index.tsx
@@ -5,15 +5,13 @@ import { FaPlus } from "react-icons/fa";
 
 const CreateButton = () => {
   return (
-    <div>
-      <Link
-        href="/create-bookmark"
-        className="flex justify-center items-center gap-2 w-full xl:w-72 bg-gray-500 text-white mb-4 px-8 py-4 rounded-md text-2xl hover:bg-gray-400 hover:text-black duration-100"
-      >
-        <FaPlus />
-        <span>ブックマーク作成</span>
-      </Link>
-    </div>
+    <Link
+      href="/create-bookmark"
+      className="flex justify-center items-center gap-2 w-full xl:w-72 bg-gray-500 text-white mb-4 px-8 py-4 rounded-md text-2xl hover:bg-gray-400 hover:text-black duration-100"
+    >
+      <FaPlus />
+      <span>ブックマーク作成</span>
+    </Link>
   );
 };
 export default CreateButton;
