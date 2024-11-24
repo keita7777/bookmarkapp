f287ced47f1b3f3a508acdf040118b2eb974cfbf
github keita7777 <github mine0kei@gmail.com>
2024-11-25 05:58:28 +0900

    フォルダデータの型定義修正

--- components/Header/HeaderNav/FolderMenu/FolderTree/index.tsx
+++ components/Header/HeaderNav/FolderMenu/FolderTree/index.tsx
@@ -2,9 +2,10 @@
 
 import { FoldersDummyData } from "@/DummtData/types/folderType";
 import FolderItem from "./FolderItem";
+import { FolderWithRelation } from "@/types/folderType";
 
 type Props = {
-  folders: FoldersDummyData;
+  folders: FolderWithRelation[];
   parentId?: string | null;
   openFolders: Record<string, boolean>;
   toggleFolder: (folderId: string) => void;
