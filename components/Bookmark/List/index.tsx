593f986a0b87526010fa01c4209d2d325c27285e
github keita7777 <github mine0kei@gmail.com>
2024-11-25 04:37:51 +0900

    eslintエラー解消

--- components/Bookmark/List/index.tsx
+++ components/Bookmark/List/index.tsx
@@ -1,20 +1,19 @@
 import { bookmarkDummyData } from "@/DummtData/bookmarkData";
 import BookmarkCard from "./Card";
 
-// type Props = {
-//   folderId?: string;
-// };
+type Props = {
+  folderId?: string;
+};
 
 // folderIdのpropsはデータフェッチで使用する予定
-const BookmarkList = () =>
-  // { folderId }: Props
-  {
-    const bookmarks = bookmarkDummyData;
+const BookmarkList = ({ folderId }: Props) => {
+  const bookmarks = bookmarkDummyData;
 
-    return (
-      <ul className="grid 2xl:grid-cols-2 xl:grid-cols-1 gap-4">
-        {bookmarks && bookmarks.map((bookmark) => <BookmarkCard key={bookmark.id} bookmark={bookmark} />)}
-      </ul>
-    );
-  };
+  return (
+    <ul className="grid 2xl:grid-cols-2 xl:grid-cols-1 gap-4">
+      {bookmarks && bookmarks.map((bookmark) => <BookmarkCard key={bookmark.id} bookmark={bookmark} />)}
+      {folderId} {/* eslintエラー対策、一時的に記述 */}
+    </ul>
+  );
+};
 export default BookmarkList;
