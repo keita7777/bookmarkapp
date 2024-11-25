5ab119bb7870cd72a9c4564d7dbd1bdcc4a84fa5
github keita7777 <github mine0kei@gmail.com>
2024-11-26 06:51:40 +0900

    編集対象のフォルダに応じて選択できる親フォルダを変更

--- components/Form/FolderForm/index.tsx
+++ components/Form/FolderForm/index.tsx
@@ -5,21 +5,22 @@
 import { FolderWithRelation } from "@/types/folderType";
 import { createFolder } from "@/utils/db/fetchData";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
 type Props = {
   folderData: FolderWithRelation[];
+  folderId?: string;
 };
 
-const FolderForm = ({ folderData }: Props) => {
+const FolderForm = ({ folderData, folderId }: Props) => {
   const router = useRouter();
   const {
     handleSubmit,
     register,
     setError,
     // resetField,
-    // setValue,
+    setValue,
     watch,
   } = useForm({
     defaultValues: {
@@ -53,6 +54,84 @@ const FolderForm = ({ folderData }: Props) => {
     }
   }, [currentParentFolderValues, folderData, setError]);
 
+  // 編集の場合ここから
+  // フォルダ名と親フォルダの初期値を設定
+  const [parentFolderId, setParentFolderId] = useState<string | null>(null);
+  const [folderName, setFolderName] = useState<string | null>(null);
+
+  // propsで渡されたfolderIdをもとにフォルダ名と親フォルダを設定
+  const getParentFolder = useCallback(() => {
+    const result = folderData.filter((item) => item.id === folderId);
+
+    if (result.length <= 0) return null;
+
+    setParentFolderId(result[0].parent_relation.parent_folder);
+    setFolderName(result[0].name);
+  }, [folderData, folderId]);
+
+  useEffect(() => {
+    // 編集の場合
+    if (folderId) {
+      getParentFolder();
+
+      if (parentFolderId !== null) {
+        setValue("parentFolder", parentFolderId);
+      }
+      if (folderName !== null) {
+        setValue("name", folderName);
+      }
+    }
+  }, [parentFolderId, folderName, setValue, folderId, getParentFolder]);
+
+  // propsで渡されたfolderIdをもとに子フォルダを取得
+  // 親フォルダを変更した際に階層が3を超えないようにする対策
+  const [formattedFolderData, setFormattedFolderData] = useState<FolderWithRelation[]>([]);
+
+  useEffect(() => {
+    if (folderId) {
+      // 編集の場合
+
+      // resultを親フォルダに持つフォルダを取得
+      const folderArray = folderData.filter((folder) => folder.parent_relation.parent_folder === folderId);
+
+      // resultを親フォルダに持つフォルダを親フォルダに持つフォルダを取得
+      const folderArray2 = folderArray.flatMap((folder) =>
+        folderData.filter((item) => item.parent_relation.parent_folder === folder.id),
+      );
+
+      if (folderArray2.length > 0) {
+        // 孫フォルダがある場合は親フォルダを選択できない
+        setFormattedFolderData([]);
+      } else if (folderArray.length > 0) {
+        // 子フォルダがある場合は第1階層のフォルダのみ選択可能
+        setFormattedFolderData(folderData.filter((item) => item.parent_relation.level === "ONE"));
+      } else {
+        // 子フォルダがない場合は第3階層のフォルダ以外選択可能
+        setFormattedFolderData(folderData.filter((item) => item.parent_relation.level !== "THREE"));
+      }
+    } else {
+      // 新規作成の場合、第3階層のフォルダ以外選択可能
+      setFormattedFolderData(folderData.filter((item) => item.parent_relation.level !== "THREE"));
+    }
+  }, [folderId, folderData]);
+
+  //     // 編集の場合
+  //     // resultを親フォルダに持つフォルダを取得
+  //     const folderArray = folderData.filter((folder) => folder.parent_relation.parent_folder === parentFolderId);
+  //     // resultを親フォルダに持つフォルダを親フォルダに持つフォルダを取得
+  //     const folderArray2 = folderArray.map((folder) => {
+  //       folderData.filter((item) => item.parent_relation.parent_folder === folder.id);
+  //     });
+  //     console.log(folderArray2);
+  //   } else {
+  //     // 新規作成の場合
+  //     console.log("folderArray2");
+  //     item.parent_relation.level !== "THREE";
+  //   }
+  // });
+
+  // 編集の場合ここまで
+
   // キャンセルボタンクリック時の処理
   const handleCancel = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -78,29 +157,19 @@ const FolderForm = ({ folderData }: Props) => {
         <label htmlFor="folder" className="text-xl font-bold">
           親フォルダを選択
         </label>
-        <select
-          className="border border-black rounded-md p-2"
-          // defaultValue="75f2efe8-e3c2-441a-9190-27c94d740f47"
-          {...register("parentFolder")}
-        >
+        <select className="border border-black rounded-md p-2" {...register("parentFolder")}>
           <option value="">指定しない</option>
           {folderData &&
-            folderData
-              .filter(
-                (folder) =>
-                  // 第3階層のフォルダ以下には作成できない
-                  folder.parent_relation.level !== "THREE",
-              )
-              .map((folder) => (
-                <option key={folder.id} value={folder.id}>
-                  {folder.name}
-                </option>
-              ))}
+            formattedFolderData.map((folder) => (
+              <option key={folder.id} value={folder.id}>
+                {folder.name}
+              </option>
+            ))}
         </select>
       </div>
       <div className="flex gap-6 justify-center items-center w-full xl:w-[400px] mx-auto">
         <button type="submit" className="rounded-md bg-gray-300 w-1/2 text-xl font-bold py-2">
-          作成
+          {folderId ? "更新" : "作成"}
         </button>
         <button type="button" onClick={handleCancel} className="rounded-md bg-gray-300 w-1/2 text-xl font-bold py-2">
           キャンセル
