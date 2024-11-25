3e7c5847429742500da14628b055d1c325bc8017
github keita7777 <github mine0kei@gmail.com>
2024-11-25 13:09:51 +0900

    ブックマーク編集用のAPIエンドポイント

--- app/api/bookmark/route.ts
+++ app/api/bookmark/route.ts
@@ -1,8 +1,8 @@
 import prisma from "@/utils/db/db";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (request: NextRequest) => {
-  const searchParams = request.nextUrl.searchParams;
+export const GET = async (req: NextRequest) => {
+  const searchParams = req.nextUrl.searchParams;
   const bookmarkId = searchParams.get("bookmarkId");
 
   try {
@@ -21,7 +21,7 @@ export const GET = async (request: NextRequest) => {
   }
 };
 
-export const POST = async (req: Request) => {
+export const POST = async (req: NextRequest) => {
   try {
     const data = await req.json();
 
@@ -50,5 +50,32 @@ export const POST = async (req: Request) => {
     return NextResponse.json({ message: "投稿完了" }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "投稿失敗", error }, { status: 500 });
+  }
+};
+
+export const PUT = async (req: NextRequest) => {
+  const searchParams = req.nextUrl.searchParams;
+  const bookmarkId = searchParams.get("bookmarkId");
+  const data = await req.json();
+
+  try {
+    await prisma.bookmarks.update({
+      where: {
+        id: bookmarkId!,
+      },
+      data: {
+        user_id: data.userId,
+        folder_id: data.folder_id,
+        url: data.url,
+        title: data.title,
+        description: data.description,
+        image: data.image,
+      },
+    });
+    return NextResponse.json({ message: "更新完了" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json({ message: "更新失敗", error }, { status: 500 });
   }
 };
