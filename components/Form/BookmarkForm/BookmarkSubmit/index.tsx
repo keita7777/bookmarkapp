c518ddc022e6ab70843a93e2e8ef7b2ada27878f
github keita7777 <github mine0kei@gmail.com>
2024-11-25 13:15:45 +0900

    編集完了後トップページに遷移

--- components/Form/BookmarkForm/BookmarkSubmit/index.tsx
+++ components/Form/BookmarkForm/BookmarkSubmit/index.tsx
@@ -139,6 +139,8 @@ const BookmarkSubmit = ({ urlData, folderData, bookmarkData }: Props) => {
       // bookmarksテーブルにデータを挿入する処理を実行
       createBookmark(urlData.url, title, description, selectedFolder, urlData.image, memo);
     }
+    router.push("/");
+    router.refresh();
   };
 
   if (!isReady) {
