6a1febf0dd3f8fd6ed7e282dd640fe4086382006
github keita7777 <github mine0kei@gmail.com>
2024-11-26 18:07:00 +0900

    propsで受け取ったfolderDataをもとに子フォルダと孫フォルダを取得

--- components/Header/HeaderNav/FolderMenu/DeleteModal/index.tsx
+++ components/Header/HeaderNav/FolderMenu/DeleteModal/index.tsx
@@ -1,4 +1,7 @@
+import { FolderWithRelation } from "@/types/folderType";
 import { getBreadcrumbPath } from "@/utils/common/breadcrumbs";
+import { deleteFolder } from "@/utils/db/fetchData";
+import { useRouter } from "next/navigation";
 import { Dispatch, SetStateAction, useEffect } from "react";
 
 type Props = {
@@ -6,9 +9,23 @@ type Props = {
   folderName: string;
   hasChild: boolean;
   setIsDeleteClick: Dispatch<SetStateAction<boolean>>;
+  folderData: FolderWithRelation[];
 };
 
-const DeleteModal = ({ id, folderName, hasChild, setIsDeleteClick }: Props) => {
+const DeleteModal = ({ id, folderName, hasChild, setIsDeleteClick, folderData }: Props) => {
+  const router = useRouter();
+
+  // 子フォルダを取得
+  const childFolders = folderData.filter((folder) => folder.parent_relation.parent_folder === id);
+
+  // 孫フォルダを取得
+  const grandChildFolders = childFolders.flatMap((folder) =>
+    folderData.filter((item) => item.parent_relation.parent_folder === folder.id),
+  );
+
+  // 削除対象のフォルダ、子フォルダ、孫フォルダをrelatedFoldersにまとめる
+  const relatedFolders = [...[...childFolders, ...grandChildFolders].map((item) => item.id), id];
+
   return (
     <div className="delete-modal flex justify-start items-center flex-col gap-4 w-full h-full bg-red-800 bg-opacity-80 absolute left-0 top-0 pt-20 z-30 text-center">
       <p className="text-xl font-bold text-white">本当に「{folderName}」フォルダを削除しますか？</p>
@@ -16,11 +33,11 @@ const DeleteModal = ({ id, folderName, hasChild, setIsDeleteClick }: Props) => {
       <div className="min-w-72 flex gap-4">
         <button
           className="bg-red-700 rounded-md px-2 py-1 w-1/2 text-white hover:bg-red-600"
-          // onClick={async () => {
-          //   await deleteBookmark(id);
-          //   setIsDeleteClick(false);
-          //   router.refresh();
-          // }}
+          onClick={async () => {
+            await deleteFolder(id, relatedFolders);
+            setIsDeleteClick(false);
+            router.refresh();
+          }}
         >
           削除
         </button>
