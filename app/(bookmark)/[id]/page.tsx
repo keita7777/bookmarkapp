4464897b02d1ae10ee9643ec2cd6981bc00632b5
github keita7777 <github mine0kei@gmail.com>
2024-11-27 15:46:36 +0900

    クエリパラメータのpageを取得

--- app/(bookmark)/[id]/page.tsx
+++ app/(bookmark)/[id]/page.tsx
@@ -6,14 +6,21 @@ import BookmarkList from "@/components/Bookmark/List";
 import Pagenation from "@/components/Bookmark/Pagenation";
 import { countBookmarks } from "@/utils/db/fetchData";
 
-export default async function BookmarksByFolderPage({ params }: { params: { id: string } }) {
+export default async function BookmarksByFolderPage({
+  params,
+  searchParams,
+}: {
+  params: { id: string };
+  searchParams: { page: string };
+}) {
   const bookmarkCount = await countBookmarks(params.id);
+  const page = searchParams.page ? Number(searchParams.page) : 1;
 
   return (
     <>
       <Breadcrumb id={params.id} />
-      <BookmarkList folderId={params.id} />
-      <Pagenation bookmarkCount={bookmarkCount} />
+      <BookmarkList folderId={params.id} page={page} />
+      <Pagenation bookmarkCount={bookmarkCount} currentPage={page} />
     </>
   );
 }
--- app/(bookmark)/page.tsx
+++ app/(bookmark)/page.tsx
@@ -5,14 +5,15 @@ import BookmarkList from "@/components/Bookmark/List";
 import Pagenation from "@/components/Bookmark/Pagenation";
 import { countBookmarks } from "@/utils/db/fetchData";
 
-export default async function Home() {
+export default async function Home({ searchParams }: { searchParams: { page: string } }) {
   const bookmarkCount = await countBookmarks();
+  const page = searchParams.page ? Number(searchParams.page) : 1;
 
   return (
     <>
       <CreateButton />
-      <BookmarkList />
-      <Pagenation bookmarkCount={bookmarkCount} />
+      <BookmarkList page={page} />
+      <Pagenation bookmarkCount={bookmarkCount} currentPage={page} />
     </>
   );
 }
