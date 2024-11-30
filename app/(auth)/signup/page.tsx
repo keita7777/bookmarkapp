97f3accad75d9c554ac04d5f2d6d20d258bef78e
github keita7777 <github mine0kei@gmail.com>
2024-11-30 15:11:10 +0900

    SignupFormを@/components/Form以下に移動

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,6 +1,6 @@
 // 新規ユーザー登録画面
 
-import SignupForm from "@/components/auth/SignupForm";
+import SignupForm from "@/components/Form/SignupForm";
 
 export default function SignupPage() {
   return <SignupForm />;
