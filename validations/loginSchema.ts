4aaff0cadcd9f278430079c60da0aa0593016eb2
github keita7777 <github mine0kei@gmail.com>
2024-11-30 15:42:21 +0900

    ログインフォームのバリデーション

---
+++ validations/loginSchema.ts
@@ -0,0 +1,12 @@
+// ログインフォームのバリデーション
+
+import { z } from "zod";
+import { passwordSchema } from "./passwordSchema";
+
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .min(1, "メールアドレスを入力してください")
+    .email({ message: "正しいメールアドレスを入力してください。" }),
+  password: passwordSchema,
+});
