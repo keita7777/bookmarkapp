275fd890286da107822657457cdb24e4db802545
github keita7777 <github mine0kei@gmail.com>
2024-12-03 23:20:12 +0900

    folderDataを取得しURLが有効の場合パンくずデータを取得する

--- app/(bookmark)/[id]/page.tsx
+++ app/(bookmark)/[id]/page.tsx
@@ -4,6 +4,8 @@
 import Breadcrumb from "@/components/Bookmark/Breadcrumb";
 import BookmarkList from "@/components/Bookmark/List";
 import Pagenation from "@/components/Bookmark/Pagenation";
+import { Folder } from "@/DummtData/types/folderType";
+import { getBreadcrumbPath } from "@/utils/common/breadcrumbs";
 import { countBookmarks, getFolderData } from "@/utils/db/fetchData";
 import { notFound } from "next/navigation";
 
@@ -18,15 +20,23 @@ export default async function BookmarksByFolderPage({
   const query = searchParams.query ? searchParams.query : undefined;
   const bookmarkCount = await countBookmarks(params.id, query);
 
-  // 無効なURLの場合not-foundページを表示させる
-  const folderData = await getFolderData(params.id);
-  if (folderData.length <= 0) {
+  const folderData = await getFolderData();
+  const folderIds = folderData.map((item: Folder) => item.id);
+
+  // パンくずデータを用意
+  let breadcrumbData = null;
+
+  if (!folderIds.includes(params.id)) {
+    // 無効なURLの場合not-foundページを表示させる
     notFound();
+  } else {
+    // 有効なURLの場合パンくずデータを取得する
+    breadcrumbData = getBreadcrumbPath(folderData, params.id);
   }
 
   return (
     <>
-      <Breadcrumb id={params.id} />
+      <Breadcrumb breadcrumbData={breadcrumbData} />
       <BookmarkList folderId={params.id} page={page} query={query} />
       <Pagenation bookmarkCount={bookmarkCount} currentPage={page} />
     </>
